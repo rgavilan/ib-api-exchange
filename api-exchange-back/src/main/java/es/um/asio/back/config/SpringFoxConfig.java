8b1a84f35ab70fc0023bcd5d39535d4b8042688e
Alejandro Martino Uria <amartino@izertis.com>
2020-11-30 17:22:26 +0100

    SEMANTMURC-383 fix swagger

---
+++ api-exchange-back/src/main/java/es/um/asio/back/config/SpringFoxConfig.java
@@ -0,0 +1,10 @@
+package es.um.asio.back.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+}
